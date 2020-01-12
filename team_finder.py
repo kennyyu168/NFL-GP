1a99d7bfbec00a103d6733c31407d85b232dd47e
Alan Tsui <alantsui18@gmail.com>
2020-01-11 22:28:57 -0800

    wait

--- team_finder.py
+++ team_finder.py
@@ -12,31 +12,3 @@ import csv
 # Parameters: team - team name
 #             file_read - file to be searched
 # Return Value: Array of values from that row
-<<<<<<< HEAD
-=======
-def find_row(team, file_read):
-	for row in file_read:
-		if team == row[0]:
-			return row
-
-# Function Name: win_lose()
-# Function Prototype: win_lose(team1, team2)
-# Description: Calculates based on each subsequent parameter which team will
-#     win or lose
-# Parameters: team1 - the data of the first team passed in
-#             team2 - the data of the second team passed in
-# Return Value: Either 1 or 2 depending on which team will win
-	
-
-# Function Name: weight()
-# Function Prototype: weight(data)
-# Description: Calculates weight of each category on each parameter of the 
-#              team data
-# Parameter: data - the team of one team
-# Return Value: Returns a percentage based on the each category
-def weight(data):
-	# Categorize each data point
-
-
-
->>>>>>> e62fce72d8512c9c786137d644090bbd6378c552
