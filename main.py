3660d3845873e5986b07341eb106e77199e33b7e
Kenny Yu <kennyyu168@yahoo.com>
2020-09-28 16:42:16 -0700

    changed Procfile

--- main.py
+++ main.py
@@ -36,5 +36,5 @@ def main():
 def butwhy():
     return render_template('butwhy.html')
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+#if __name__ == '__main__':
+#    app.run(host='127.0.0.1', port=8000, debug=True)
